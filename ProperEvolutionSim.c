d495e5457562d13076660c035b259063d1481168
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2022-07-20 18:07:20 +0200

    Finished a lot of struct definition, initialisation and destruction

--- ProperEvolutionSim.c
+++ ProperEvolutionSim.c
@@ -29,25 +29,27 @@ typedef struct __MAIN_GeneConstraints MAIN_GeneConstraints;
 typedef struct __MAIN_UintConstraint MAIN_UintConstraint;
 typedef struct __MAIN_IntConstraint MAIN_IntConstraint;
 typedef struct __MAIN_FloatConstraint MAIN_FloatConstraint;
+typedef struct __MAIN_MapSettings MAIN_MapSettings;
+typedef struct __MAIN_Size MAIN_Size;
+typedef struct __MAIN_Map MAIN_Map;
+typedef struct __MAIN_Tile MAIN_Tile;
+typedef struct __MAIN_Plant MAIN_Plant;
 
-struct __MAIN_UintConstraint
-{
+struct __MAIN_UintConstraint {
     uint64_t min;    // The minimum allowed value
     uint64_t max;    // The maximum allowed value
     uint64_t mean;   // The mean starting value
     uint64_t spread; // The spread of the starting value
 };
 
-struct __MAIN_IntConstraint
-{
+struct __MAIN_IntConstraint {
     int64_t min;    // The minimum allowed value
     int64_t max;    // The maximum allowed value
     int64_t mean;   // The mean starting value
     int64_t spread; // The spread of the starting value
 };
 
-struct __MAIN_FloatConstraint
-{
+struct __MAIN_FloatConstraint {
     double min;    // The minimum allowed value
     double max;    // The maximum allowed value
     double mean;   // The mean starting value
@@ -72,16 +74,44 @@ struct __MAIN_GeneConstraints {
     MAIN_UintConstraint mutationAttempts; // The number of attempts to do a mutation during spawning
 };
 
-struct __MAIN_Settings
-{
+struct __MAIN_MapSettings {
+    uint64_t width; // The width of the map
+    uint64_t height; // The height of the map
+    uint64_t minEnergy; // The minimum energy production per tile
+};
+
+struct __MAIN_Settings {
     MAIN_GeneConstraints geneConstraints; // All the gene constraints
+    MAIN_MapSettings map;                 // All the map settings
     bool killIlligal;                     // If true then it should kill any plant created with illigal genes, if false they should be trunctated
+};
+
+struct __MAIN_Size {
+    uint64_t w; // The width
+    uint64_t h; // The height
+};
+
+struct __MAIN_Map {
+    MAIN_Settings *settings; // The settings of the map
+    MAIN_Tile *tiles; // All of the tiles for the map
+    MAIN_Size size; // The size of the map
+};
+
+struct __MAIN_Tile {
+    MAIN_Plant **plantList; // List of all the plants in this tile in order from tallest to lowest
+    size_t plantCount; // The number of plants in the plant list
+    uint64_t energy; // The energy production of the tile
+};
+
+struct __MAIN_Plant {
+
 };
 
 // Settings translation tables
 #define MAIN_SETTINGSCONSTRAINTCOUNT 4
 #define MAIN_SETTINGSGENECONSTRAINTCOUNT 15
-#define MAIN_SETTINGSCOUNT 2
+#define MAIN_SETTINGSCOUNT 3
+#define MAIN_SETTINGSMAPCOUNT 3
 
 SET_TranslationTable MAIN_SettingsTableUintConstraint[MAIN_SETTINGSCONSTRAINTCOUNT] = {
     {.name = "min", .type = SET_DATATYPE_UINT64, .depth = 0, .offset = offsetof(MAIN_UintConstraint, min)},
@@ -122,8 +152,15 @@ SET_TranslationTable MAIN_SettingsTableGeneConstrains[MAIN_SETTINGSGENECONSTRAIN
     {.name = "mutationAttempts", .type = SET_DATATYPE_STRUCT, .depth = 0, .offset = offsetof(MAIN_GeneConstraints, mutationAttempts), .size = sizeof(MAIN_UintConstraint), .sub = MAIN_SettingsTableUintConstraint, .count = MAIN_SETTINGSCONSTRAINTCOUNT}
 };
 
+SET_TranslationTable MAIN_SettingsTableMap[MAIN_SETTINGSMAPCOUNT] = {
+    {.name = "width", .type = SET_DATATYPE_UINT64, .depth = 0, .offset = offsetof(MAIN_MapSettings, width)},
+    {.name = "height", .type = SET_DATATYPE_UINT64, .depth = 0, .offset = offsetof(MAIN_MapSettings, height)},
+    {.name = "minEnergy", .type = SET_DATATYPE_UINT64, .depth = 0, .offset = offsetof(MAIN_MapSettings, minEnergy)}
+};
+
 SET_TranslationTable MAIN_SettingsTableMain[MAIN_SETTINGSCOUNT] = {
     {.name = "geneConstraints", .type = SET_DATATYPE_STRUCT, .depth = 0, .offset = offsetof(MAIN_Settings, geneConstraints), .size = sizeof(MAIN_GeneConstraints), .sub = MAIN_SettingsTableGeneConstrains, .count = MAIN_SETTINGSGENECONSTRAINTCOUNT},
+    {.name = "map", .type = SET_DATATYPE_STRUCT, .depth = 0, .offset = offsetof(MAIN_Settings, map), .size = sizeof(MAIN_MapSettings), .sub = MAIN_SettingsTableMap, .count = MAIN_SETTINGSMAPCOUNT},
     {.name = "killIlligal", .type = SET_DATATYPE_BOOL, .depth = 0, .offset = offsetof(MAIN_Settings, killIlligal)}
 };
 
@@ -131,11 +168,44 @@ SET_TranslationTable MAIN_SettingsTableMain[MAIN_SETTINGSCOUNT] = {
 // Load a settings file
 MAIN_Settings *MAIN_LoadSettings(const char *FileName);
 
+// Create a new map
+MAIN_Map *MAIN_CreateMap(const char *FileName);
+
 // Init functions
+void MAIN_InitUintConstraint(MAIN_UintConstraint *Struct);
+void MAIN_InitIntConstraint(MAIN_IntConstraint *Struct);
+void MAIN_InitFloatConstraint(MAIN_FloatConstraint *Struct);
+void MAIN_InitGeneConstraints(MAIN_GeneConstraints *Struct);
+void MAIN_InitMapSettings(MAIN_MapSettings *Struct);
 void MAIN_InitSettings(MAIN_Settings *Struct);
+void MAIN_InitMap(MAIN_Map *Struct);
+void MAIN_InitTile(MAIN_Tile *Struct);
+void MAIN_InitPlant(MAIN_Plant *Struct);
+void MAIN_InitSize(MAIN_Size *Struct);
+
+// Clean functions
+void MAIN_CleanUintConstraint(MAIN_UintConstraint *Struct);
+void MAIN_CleanIntConstraint(MAIN_IntConstraint *Struct);
+void MAIN_CleanFloatConstraint(MAIN_FloatConstraint *Struct);
+void MAIN_CleanGeneConstraints(MAIN_GeneConstraints *Struct);
+void MAIN_CleanMapSettings(MAIN_MapSettings *Struct);
+void MAIN_CleanSettings(MAIN_Settings *Struct);
+void MAIN_CleanMap(MAIN_Map *Struct);
+void MAIN_CleanTile(MAIN_Tile *Struct);
+void MAIN_CleanPlant(MAIN_Plant *Struct);
+void MAIN_CleanSize(MAIN_Size *Struct);
 
 // Destroy functions
+void MAIN_DestroyUintConstraint(MAIN_UintConstraint *Struct);
+void MAIN_DestroyIntConstraint(MAIN_IntConstraint *Struct);
+void MAIN_DestroyFloatConstraint(MAIN_FloatConstraint *Struct);
+void MAIN_DestroyGeneConstraints(MAIN_GeneConstraints *Struct);
+void MAIN_DestroyMapSettings(MAIN_MapSettings *Struct);
 void MAIN_DestroySettings(MAIN_Settings *Struct);
+void MAIN_DestroyMap(MAIN_Map *Struct);
+void MAIN_DestroyTile(MAIN_Tile *Struct);
+void MAIN_DestroyPlant(MAIN_Plant *Struct);
+void MAIN_DestroySize(MAIN_Size *Struct);
 
 MAIN_Settings *MAIN_LoadSettings(const char *FileName)
 {
@@ -147,6 +217,8 @@ MAIN_Settings *MAIN_LoadSettings(const char *FileName)
         _MAIN_AddErrorForeign(MAIN_ERRORID_LOADSETTINGS_MALLOC, strerror(errno), MAIN_ERRORMES_MALLOC, sizeof(MAIN_Settings));
         return NULL;
     }
+
+    MAIN_InitSettings(Settings);
 
     // Load the settings
     extern SET_TranslationTable MAIN_SettingsTableMain[];
@@ -161,7 +233,7 @@ MAIN_Settings *MAIN_LoadSettings(const char *FileName)
     }
 
     // Translate the settings
-    if (!SET_Translate(Settings, SettingsDict, MAIN_SettingsTableMain, MAIN_SETTINGSCOUNT, SET_TRANSLATIONMODE_FILL))
+    if (!SET_Translate(Settings, SettingsDict, MAIN_SettingsTableMain, MAIN_SETTINGSCOUNT, SET_TRANSLATIONMODE_NONE))
     {
         _MAIN_AddErrorForeign(MAIN_ERRORID_LOADSETTINGS_TRANSLATE, SET_GetError(), MAIN_ERRORMES_TRANSLATESETTINGS, FileName);
         SET_DestroyDataStruct(SettingsDict);
@@ -174,14 +246,307 @@ MAIN_Settings *MAIN_LoadSettings(const char *FileName)
     return Settings;
 }
 
+MAIN_Map *MAIN_CreateMap(const char *FileName)
+{
+    // Allocate memory
+
+}
+
+
+void MAIN_InitUintConstraint(MAIN_UintConstraint *Struct)
+{
+    Struct->min = 0;
+    Struct->max = 0xFFFFFFFFFFFFFFFF;
+    Struct->mean = 0;
+    Struct->spread = 0;
+}
+
+void MAIN_InitIntConstraint(MAIN_IntConstraint *Struct)
+{
+    Struct->min = -0x8000000000000000;
+    Struct->max = -0x7FFFFFFFFFFFFFFF;
+    Struct->mean = 0;
+    Struct->spread = 0;
+}
+
+void MAIN_InitFloatConstraint(MAIN_FloatConstraint *Struct)
+{
+    Struct->min = 0.;
+    Struct->max = 1.;
+    Struct->mean = 0.5;
+    Struct->spread = 0.5;
+}
+
+void MAIN_InitGeneConstraints(MAIN_GeneConstraints *Struct)
+{
+    Struct->maxHeight.min = 1;
+    Struct->maxHeight.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->maxHeight.mean = 100;
+    Struct->maxHeight.spread = 100;
+
+    Struct->maxSize.min = 1;
+    Struct->maxSize.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->maxSize.mean = 10;
+    Struct->maxSize.spread = 10;
+
+    Struct->efficiency.min = 0.;
+    Struct->efficiency.max = 1.;
+    Struct->efficiency.mean = 0.5;
+    Struct->efficiency.spread = 0.5;
+
+    Struct->growthRateHeight.min = 0.;
+    Struct->growthRateHeight.max = 1.;
+    Struct->growthRateHeight.mean = 0.5;
+    Struct->growthRateHeight.spread = 0.5;
+
+    Struct->growthRateSize.min = 0.;
+    Struct->growthRateSize.max = 1.;
+    Struct->growthRateSize.mean = 0.5;
+    Struct->growthRateSize.spread = 0.5;
+
+    Struct->minGrowthEnergyHeight.min = 0;
+    Struct->minGrowthEnergyHeight.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->minGrowthEnergyHeight.mean = 1000;
+    Struct->minGrowthEnergyHeight.spread = 1000;
+
+    Struct->minGrowthEnergySize.min = 0;
+    Struct->minGrowthEnergySize.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->minGrowthEnergySize.mean = 1000;
+    Struct->minGrowthEnergySize.spread = 1000;
+
+    Struct->spawnRate.min = 0.;
+    Struct->spawnRate.max = 1.;
+    Struct->spawnRate.mean = 0.5;
+    Struct->spawnRate.spread = 0.5;
+
+    Struct->minSpawnEnergy.min = 0;
+    Struct->minSpawnEnergy.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->minSpawnEnergy.mean = 1000;
+    Struct->minSpawnEnergy.spread = 1000;
+
+    Struct->maxTileEnergy.min = 1;
+    Struct->maxTileEnergy.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->maxTileEnergy.mean = 1000;
+    Struct->maxTileEnergy.spread = 1000;
+
+    Struct->spawnEnergy.min = 1;
+    Struct->spawnEnergy.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->spawnEnergy.mean = 100;
+    Struct->spawnEnergy.spread = 100;
+
+    Struct->spawnSize.min = 1;
+    Struct->spawnSize.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->spawnSize.mean = 5;
+    Struct->spawnSize.spread = 5;
+
+    Struct->spawnSpread.min = 0;
+    Struct->spawnSpread.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->spawnSpread.mean = 5;
+    Struct->spawnSpread.spread = 5;
+
+    Struct->mutationRate.min = 0.;
+    Struct->mutationRate.max = 1.;
+    Struct->mutationRate.mean = 0.5;
+    Struct->mutationRate.spread = 0.5;
+
+    Struct->mutationAttempts.min = 0;
+    Struct->mutationAttempts.max = 0xFFFFFFFFFFFFFFFF;
+    Struct->mutationAttempts.mean = 10;
+    Struct->mutationAttempts.spread = 10;
+}
+
+void MAIN_InitMapSettings(MAIN_MapSettings *Struct)
+{
+    Struct->height = 1024;
+    Struct->width = 1024;
+    Struct->minEnergy = 1000;
+}
+
 void MAIN_InitSettings(MAIN_Settings *Struct)
 {
+    MAIN_InitGeneConstraints(&Struct->geneConstraints);
+    MAIN_InitMapSettings(&Struct->map);
+    Struct->killIlligal = true;
+}
+
+void MAIN_InitMap(MAIN_Map *Struct)
+{
+    Struct->settings = NULL;
+    MAIN_InitSize(&Struct->size);
+    Struct->tiles = NULL;
+}
+
+void MAIN_InitTile(MAIN_Tile *Struct)
+{
+    Struct->energy = 0;
+    Struct->plantCount = 0;
+    Struct->plantList = NULL;
+}
+
+void MAIN_InitPlant(MAIN_Plant *Struct)
+{
+
+}
+
+void MAIN_InitSize(MAIN_Size *Struct)
+{
+    Struct->w = 0;
+    Struct->h = 0;
+}
+
+
+void MAIN_CleanUintConstraint(MAIN_UintConstraint *Struct)
+{
+
+}
+
+void MAIN_CleanIntConstraint(MAIN_IntConstraint *Struct)
+{
+
+}
+
+void MAIN_CleanFloatConstraint(MAIN_FloatConstraint *Struct)
+{
+
+}
+
+void MAIN_CleanGeneConstraints(MAIN_GeneConstraints *Struct)
+{
+    // Clean all constraints
+    MAIN_CleanUintConstraint(&Struct->maxHeight);
+    MAIN_CleanUintConstraint(&Struct->maxSize);
+    MAIN_CleanFloatConstraint(&Struct->efficiency);
+    MAIN_CleanFloatConstraint(&Struct->growthRateHeight);
+    MAIN_CleanFloatConstraint(&Struct->growthRateSize);
+    MAIN_CleanUintConstraint(&Struct->minGrowthEnergyHeight);
+    MAIN_CleanUintConstraint(&Struct->minGrowthEnergySize);
+    MAIN_CleanFloatConstraint(&Struct->spawnRate);
+    MAIN_CleanUintConstraint(&Struct->minSpawnEnergy);
+    MAIN_CleanUintConstraint(&Struct->maxTileEnergy);
+    MAIN_CleanUintConstraint(&Struct->spawnEnergy);
+    MAIN_CleanUintConstraint(&Struct->spawnSize);
+    MAIN_CleanUintConstraint(&Struct->spawnSpread);
+    MAIN_CleanFloatConstraint(&Struct->mutationRate);
+    MAIN_CleanUintConstraint(&Struct->mutationAttempts);
+}
+
+void MAIN_CleanMapSettings(MAIN_MapSettings *Struct)
+{
+
+}
+
+void MAIN_CleanSettings(MAIN_Settings *Struct)
+{
+    // Clean gene constraints
+    MAIN_CleanGeneConstraints(&Struct->geneConstraints);
+
+    // Clean map settings
+    MAIN_CleanMapSettings(&Struct->map);
+}
+
+void MAIN_CleanMap(MAIN_Map *Struct)
+{
+    // Destroy settings
+    if (Struct->settings != NULL)
+        MAIN_DestroySettings(Struct->settings);
+
+    // Destroy tiles
+    if (Struct->tiles != NULL)
+    {
+        for (MAIN_Tile *TileList = Struct->tiles, *EndTileList = Struct->tiles + Struct->size.w * Struct->size.h; TileList < EndTileList; ++TileList)
+            MAIN_CleanTile(TileList);
+
+        free(Struct->tiles);
+    }
+
+    // Clean the size
+    MAIN_CleanSize(&Struct->size);
+}
+
+void MAIN_CleanTile(MAIN_Tile *Struct)
+{
+    // Free the plants
+    if (Struct->plantList != NULL)
+    {
+        for (MAIN_Plant **List = Struct->plantList, **EndList = Struct->plantList + Struct->plantCount; List < EndList; ++List)
+            if (*List != NULL)
+                MAIN_DestroyPlant(*List);
+
+        free(Struct->plantList);
+    }
+}
+
+void MAIN_CleanPlant(MAIN_Plant *Struct)
+{
+
+}
+
+void MAIN_CleanSize(MAIN_Size *Struct)
+{
+
+}
+
+
+void MAIN_DestroyUintConstraint(MAIN_UintConstraint *Struct)
+{
+    MAIN_CleanUintConstraint(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroyIntConstraint(MAIN_IntConstraint *Struct)
+{
+    MAIN_CleanIntConstraint(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroyFloatConstraint(MAIN_FloatConstraint *Struct)
+{
+    MAIN_CleanFloatConstraint(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroyGeneConstraints(MAIN_GeneConstraints *Struct)
+{
+    MAIN_CleanGeneConstraints(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroyMapSettings(MAIN_MapSettings *Struct)
+{
+    MAIN_CleanMapSettings(Struct);
+    free(Struct);
 }
 
 void MAIN_DestroySettings(MAIN_Settings *Struct)
 {
+    MAIN_CleanSettings(Struct);
     free(Struct);
 }
+
+void MAIN_DestroyMap(MAIN_Map *Struct)
+{
+    MAIN_CleanMap(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroyTile(MAIN_Tile *Struct)
+{
+    MAIN_CleanTile(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroyPlant(MAIN_Plant *Struct)
+{
+    MAIN_CleanPlant(Struct);
+    free(Struct);
+}
+
+void MAIN_DestroySize(MAIN_Size *Struct)
+{
+    MAIN_CleanSize(Struct);
+    free(Struct);
+}
+
 
 // Main program
 int main(int argc, char **argv)
